ee95790ac38f9e26dae1d3fe8f8b00a032816fe4
prasadganta <35609156+prasadganta@users.noreply.github.com>
2020-04-16 07:32:39 +0530

    Removed git comments

--- StatusTrackerApi/src/main/java/com/statustracker/springboot/StatusTrackerApplication.java
+++ StatusTrackerApi/src/main/java/com/statustracker/springboot/StatusTrackerApplication.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.statustracker.springboot;
 
 import org.springframework.boot.SpringApplication;
@@ -25,31 +24,3 @@ public class StatusTrackerApplication extends SpringBootServletInitializer {
 		SpringApplication.run(StatusTrackerApplication.class, args);
 	}
 }
-=======
-package com.statustracker.springboot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-
-@SpringBootApplication
-@Configuration
-@ComponentScan(basePackages = "com")
-@EnableAutoConfiguration
-public class StatusTrackerApplication extends SpringBootServletInitializer {
-
-	
-	   @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	        return application.sources(StatusTrackerApplication.class);
-	    }
-	public static void main(String[] args) {
-		SpringApplication.run(StatusTrackerApplication.class, args);
-	}
-}
->>>>>>> 44663fccec70c11d3baafbaea42169259ac2ea2d
